740bed1029618dfa33b0d31c51118c0693ef512e
CallMeDek <choidaegyu9671@likelion.org>
2018-04-05 16:42:31 +0900

    UI of LogIn page has been changed

---
+++ 최대규/AndroidStudioProjects/DoitMission6/DoitMission6/app/src/main/java/org/androidtown/doitmission6/MenuActivity.java
@@ -0,0 +1,94 @@
+package org.androidtown.doitmission6;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private static final int RESULT_CODE_SUB1 = 11;
+    private static final int RESULT_CODE_SUB2 = 12;
+    private static final int RESULT_CODE_SUB3 = 13;
+
+    private Button customer_info_maintain_button;
+    private Button sales_info_maintain_button;
+    private Button product_info_maintain_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        customer_info_maintain_button = findViewById(R.id.button);
+        sales_info_maintain_button = findViewById(R.id.button2);
+        product_info_maintain_button = findViewById(R.id.button3);
+
+        customer_info_maintain_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(), sub1Activity.class);
+                startActivityForResult(intent1, RESULT_CODE_SUB1);
+            }
+        });
+
+        sales_info_maintain_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getApplicationContext(), sub2.class);
+                startActivityForResult(intent2, RESULT_CODE_SUB2);
+            }
+        });
+
+        product_info_maintain_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(getApplicationContext(), sub3.class);
+                startActivityForResult(intent3, RESULT_CODE_SUB3);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case RESULT_CODE_SUB1:
+                if (resultCode == RESULT_OK){
+                    try{
+                        Bundle bundle = data.getExtras();
+                        String name = bundle.getString("name_sub1");
+                        Toast.makeText(this, name + " 메뉴에서 응답함.", Toast.LENGTH_LONG).show();
+                    } catch (NullPointerException e){
+                        Log.e("MenuActivity", "Null intent를 참조하였습니다");
+                    }
+                }
+                break;
+            case RESULT_CODE_SUB2:
+                if (resultCode == RESULT_OK){
+                    try{
+                        Bundle bundle = data.getExtras();
+                        String name = bundle.getString("name_sub2");
+                        Toast.makeText(this, name + " 메뉴에서 응답함.", Toast.LENGTH_LONG).show();
+                    } catch (NullPointerException e){
+                        Log.e("MenuActivity", "Null intent를 참조하였습니다");
+                    }
+                }
+                break;
+            case RESULT_CODE_SUB3:
+                if (resultCode == RESULT_OK){
+                    try{
+                        Bundle bundle = data.getExtras();
+                        String name = bundle.getString("name_sub3");
+                        Toast.makeText(this, name + " 메뉴에서 응답함.", Toast.LENGTH_LONG).show();
+                    } catch (NullPointerException e){
+                        Log.e("MenuActivity", "Null intent를 참조하였습니다");
+                    }
+                }
+                break;
+        }
+    }
+}
