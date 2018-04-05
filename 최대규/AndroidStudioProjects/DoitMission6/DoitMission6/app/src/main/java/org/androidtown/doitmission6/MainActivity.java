be5707c724d8a158d5a84fc93d47ef55961a4b4b
CallMeDek <choidaegyu9671@likelion.org>
2018-04-05 14:33:38 +0900

    LogIn page has been implemented

---
+++ 최대규/AndroidStudioProjects/DoitMission6/DoitMission6/app/src/main/java/org/androidtown/doitmission6/MainActivity.java
@@ -0,0 +1,62 @@
+package org.androidtown.doitmission6;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RESULT_CODE = 1;
+    private EditText id_text;
+    private EditText pass_text;
+    private Button login_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         id_text = findViewById(R.id.editText);
+         pass_text = findViewById(R.id.editText2);
+         login_button = findViewById(R.id.button2);
+
+         login_button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 if(id_text.getText().toString() == null){
+                     Toast.makeText(MainActivity.this, "ID를 입력해주세요.", Toast.LENGTH_LONG).show();
+                 }
+                 else if(pass_text.getText().toString() == null){
+                     Toast.makeText(MainActivity.this, "PASSWORD를 입력해주세요.", Toast.LENGTH_LONG).show();
+                 }
+                 else{
+                     Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                     startActivityForResult(intent, RESULT_CODE);
+                 }
+             }
+         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case RESULT_CODE:
+                if(resultCode == RESULT_OK){
+                    try{
+                        Bundle bundle = data.getExtras();
+                        String name = bundle.getString("name");
+                        Toast.makeText(this, name + " 메뉴에서 응답함.", Toast.LENGTH_LONG).show();
+                    } catch (NullPointerException e){
+                        Log.e("MainActivity", "Null intent를 참조하였습니다");
+                    }
+                }
+        }
+    }
+}
